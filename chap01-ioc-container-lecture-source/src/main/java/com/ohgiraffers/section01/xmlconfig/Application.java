330751386e60fb5bfc9c0cc0be6593cf4adc18ab
weed97 <weed9935@gmail.com>
2024-07-04 15:48:54 +0900

    first commit

---
+++ chap01-ioc-container-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/Application.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.section01.xmlconfig;
+
+import com.ohgiraffers.common.MemberDTO;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new GenericXmlApplicationContext("section01/xmlconfig/spring-context.xml");
+
+
+//        MemberDTO memberDTO = new MemberDTO();
+        // ID(이름)으로 값 꺼내오기
+        MemberDTO member =(MemberDTO)context.getBean("member");
+
+        // 타입으로 값 꺼내오기
+
+        /*타입으로 꺼내올 때는, MeberDTO 타입으로 등록된 빈이 하나가 아닐 수 있기 때문에
+        * 모호성 에러가 발생 할 수 있다.*/
+        MemberDTO member2 = context.getBean(MemberDTO.class);
+
+        MemberDTO member3 = context.getBean("member",MemberDTO.class);
+
+        System.out.println(member3);
+    }
+}
